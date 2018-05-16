9994df4c49bdeffcc618387db4eb2a5b074cd162
g198422 <g198422@LI009>
2018-05-15 21:03:10 -0300

    dois

--- src/teste4/Teste4.java
+++ src/teste4/Teste4.java
@@ -15,7 +15,7 @@ public class Teste4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Mudança");
     }
     
 }
